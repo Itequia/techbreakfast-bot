627a8b5853b0d5a2cec9e8fe1c6b0cf5e3b82587
Adrià Álvarez Guerra <adria.alvarez@itequia.com>
2017-11-16 08:47:24 +0100

    initial commit

---
+++ Itequia.TechBreakfast/Controllers/MessagesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Itequia.TechBreakfast.Controllers
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<Activity> HandleSystemMessage(Activity message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+
+            switch (message.Type)
+            {
+                case ActivityTypes.DeleteUserData:
+                    // Implement user deletion here
+                    // If we handle user deletion, return a real message
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    // Handle conversation state changes, like members being added and removed
+                    // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                    // Not available in all channels
+
+                    IConversationUpdateActivity update = message;
+                    var client = new ConnectorClient(new Uri(message.ServiceUrl), new MicrosoftAppCredentials());
+                    if (update.MembersAdded != null && update.MembersAdded.Any())
+                    {
+                        foreach (var newMember in update.MembersAdded)
+                        {
+                            if (newMember.Id == message.Recipient.Id) continue;
+                            var reply = message.CreateReply();
+                            reply.Text = "¡Bienvenido a nuestra tienda online! \U0001F604 \n\n ¿Puedo ayudarte a gestionar tus pedidos?";
+                            await client.Conversations.ReplyToActivityAsync(reply);
+                        }
+                    }
+                    break;
+                case ActivityTypes.ContactRelationUpdate:
+                    // Handle add/remove from contact lists
+                    // Activity.From + Activity.Action represent what happened
+                    break;
+                case ActivityTypes.Typing:
+                    // Handle knowing tha the user is typing
+                    break;
+                case ActivityTypes.Ping:
+                    break;
+                default:
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Itequia.TechBreakfast/Dialogs/RootDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+
+namespace Itequia.TechBreakfast.Dialogs
+{
+    [LuisModel("b72c0c3c-d9a4-4a3d-9711-65efbb257eb5", "9e4b7c45662243198e0319a74ec82449", domain: "westus.api.cognitive.microsoft.com")]
+    [Serializable]
+    public class RootDialog : LuisDialog<object>
+    {
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            await context.PostAsync("Lo siento, no te he entendido. Si lo necesitas, puedes pedirme ayuda.");
+        }
+        
+        [LuisIntent("order.list")]
+        public async Task ListOrders(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            await context.PostAsync("order list detected");
+        }
+
+        [LuisIntent("order.status")]
+        public async Task OrderStatus(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            await context.PostAsync("order list detected");
+        }
+
+        [LuisIntent("help")]
+        public async Task RequestHelp(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            var reply = context.MakeMessage();
+            reply.Text = "Puedo ayudarte con las siguientes tareas. ¿Qué deseas hacer?";
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction { Title = "Ver pedidos", Type = ActionTypes.ImBack, Value = "Ver pedidos" },
+                    new CardAction { Title = "Consultar estado pedido", Type = ActionTypes.ImBack, Value = "Consultar estado pedido" },
+                    new CardAction { Title = "Crear nuevo pedido", Type = ActionTypes.ImBack, Value = "Crear nuevo pedido" }
+                }
+            };
+
+            await context.PostAsync(reply, CancellationToken.None);
+            context.Done(string.Empty);
+        }
+    }
+}
